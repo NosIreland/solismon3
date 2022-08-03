f561f24ca208f5fb61be361aa0720a003a5b921e
Jason Smyth <slayer1551@gmail.com>
2022-08-03 17:32:16 +0100

    password auth

--- config/config.py
+++ config/config.py
@@ -3,6 +3,8 @@ INVERTER_IP = "192.168.1.55"  # IP address of inverter
 INVERTER_PORT = 8899          # Port number
 MQTT_SERVER = "192.168.1.20"  # IP address of MQTT server
 MQTT_PORT = 1883              # Port number of MQTT server
+MQTT_SERVER_USER = "mqtt"     # MQTT server username
+MQTT_SERVER_PASSWORD = "1234" # MQTT server password
 MQTT_TOPIC = "solis/METRICS"  # MQTT topic to use
 CHECK_INTERVAL = 30           # How often to check(seconds), only applies when 'PROMETHEUS = False' otherwise uses Prometheus scrape interval
 MQTT_KEEPALIVE = 60           # MQTT keepalive
